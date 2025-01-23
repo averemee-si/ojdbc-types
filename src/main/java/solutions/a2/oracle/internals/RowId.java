98eb9be0f4c203cd03c1b939ae23ff45f2cb6944
averemee <averemee@a2.solutions>
2025-01-23 20:32:35 +0100

    add final to RowId constants

--- src/main/java/solutions/a2/oracle/internals/RowId.java
+++ src/main/java/solutions/a2/oracle/internals/RowId.java
@@ -26,8 +26,8 @@ import java.util.Objects;
  */
 public class RowId implements Serializable {
 
-	public static RowId ZERO = new RowId(0, 0, (short) 0);
-	public static int BYTES = 10;
+	public static final RowId ZERO = new RowId(0, 0, (short) 0);
+	public static final int BYTES = 10;
 
 	private static final long serialVersionUID = 5763607360618681704L;
 	private static final int ROWID_SIZE = 18;
